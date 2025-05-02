3171741f923d9d90d67abbf2e738810e7440749b
flekke <87627091+flekke@users.noreply.github.com>
2025-05-02 16:08:05 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,6 @@ resource "openstack_compute_instance_v2" "vm" {
   name         = "vm-${count.index}"  # 이름: vm-0, vm-1, ...
   flavor_name  = "aem.1c2r.50g"  # 원하는 flavor 이름
   key_pair     = "newkey"  # OpenStack에 등록된 SSH 키 이름
-  security_groups = ["ssh"]  # SSH 허용 시큐리티 그룹
 
   block_device {
     uuid = "44cae3e6-0907-4d6b-8376-2858e234f4d8"
