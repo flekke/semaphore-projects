f2fff70a71ee8103f23cb7210bf4aadaa2337a0d
flekke <87627091+flekke@users.noreply.github.com>
2025-05-03 21:18:57 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,22 +11,66 @@ provider "openstack" {
   cloud = "openstack" # ~/.config/openstack/clouds.yaml 기준
 }
 
-resource "openstack_compute_instance_v2" "vm" {
-  count        = 3  # VM 3개 생성
-  name         = "vm-${count.index}"  # 이름: vm-0, vm-1, ...
-  flavor_name  = "aem.1c2r.50g"  # 원하는 flavor 이름
-  key_pair     = "newkey"  # OpenStack에 등록된 SSH 키 이름
+resource "openstack_compute_instance_v2" "vm0" {  # 첫 번째 VM
+  name         = "vm-0"  # 이름
+  flavor_name  = "aem.1c2r.50g"  # VM 크기 설정
+  key_pair     = "newkey"  # 사용할 SSH 키
 
   block_device {
-  boot_index            = 0
-  delete_on_termination = true
-  destination_type      = "volume"
-  source_type           = "image"
-  uuid                  = "44cae3e6-0907-4d6b-8376-2858e234f4d8"
-  volume_size           = 10  # ← 이 부분을
+    boot_index            = 0  # 부팅 순서
+    delete_on_termination = true  # VM 삭제 시 볼륨도 삭제
+    destination_type      = "volume"  # 볼륨 부팅
+    source_type           = "image"  # 이미지로부터 생성
+    uuid                  = "44cae3e6-0907-4d6b-8376-2858e234f4d8"  # 사용할 이미지 UUID
+    volume_size           = 10  # 볼륨 크기 (GB)
+  }
+
+  network {
+    name = "oslomet"  # 연결할 네트워크 이름
+  }
 }
+
+resource "openstack_compute_instance_v2" "vm1" {  # 두 번째 VM
+  name         = "vm-1"
+  flavor_name  = "aem.1c2r.50g"
+  key_pair     = "newkey"
+
+  block_device {
+    boot_index            = 0
+    delete_on_termination = true
+    destination_type      = "volume"
+    source_type           = "image"
+    uuid                  = "44cae3e6-0907-4d6b-8376-2858e234f4d8"
+    volume_size           = 10
+  }
 
   network {
     name = "oslomet"
   }
+
+  depends_on = [openstack_compute_instance_v2.vm0]  # vm0이 먼저 생성된 후 실행
 }
+
+resource "openstack_compute_instance_v2" "vm2" {  # 세 번째 VM
+  name         = "vm-2"
+  flavor_name  = "aem.1c2r.50g"
+  key_pair     = "newkey"
+
+  block_device {
+    boot_index            = 0
+    delete_on_termination = true
+    destination_type      = "volume"
+    source_type           = "image"
+    uuid                  = "44cae3e6-0907-4d6b-8376-2858e234f4d8"
+    volume_size           = 10
+  }
+
+  network {
+    name = "oslomet"
+  }
+
+  depends_on = [openstack_compute_instance_v2.vm1]  # vm1 생성 후 실행
+}
+
+
+
